22d7f8c532435d09abd0a9f6867bb3fa78890bdb
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2023-08-09 00:36:59 +0530

    Updated Home Page two more contents

--- src/pages/FooterPage.vue
+++ src/pages/FooterPage.vue
@@ -1,6 +1,11 @@
 <template>
   <q-footer
-    style="background-color: rgb(189 186 16); position: absolute; bottom: 0"
+    style="
+      background-color: #ffc000;
+      color: black;
+      position: absolute;
+      bottom: 0;
+    "
   >
     <q-toolbar class="footer-text-class">
       <p class="q-mt-md text-class">
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -16,7 +16,12 @@
       >
         <div class="absolute-bottom custom-caption">
           <div class="text-h2">Ghee Sweets</div>
-          <div class="text-subtitle1">Shop Now</div>
+          <div class="text-subtitle1">
+            <q-btn
+              label="Shop Now"
+              style="color: white; background-color: rgb(221, 161, 161)"
+            />
+          </div>
         </div>
       </q-carousel-slide>
       <q-carousel-slide
@@ -25,7 +30,12 @@
       >
         <div class="absolute-bottom custom-caption">
           <div class="text-h2">Dairy Sweets</div>
-          <div class="text-subtitle1">Shop Now</div>
+          <div class="text-subtitle1">
+            <q-btn
+              label="Shop Now"
+              style="color: white; background-color: rgb(221, 161, 161)"
+            />
+          </div>
         </div>
       </q-carousel-slide>
       <q-carousel-slide
@@ -34,16 +44,25 @@
       >
         <div class="absolute-bottom custom-caption">
           <div class="text-h2">Cashew Sweets</div>
-          <div class="text-subtitle1">Shop Now</div>
+          <div class="text-subtitle1">
+            <q-btn
+              label="Shop Now"
+              style="color: white; background-color: rgb(221, 161, 161)"
+            />
+          </div>
         </div>
       </q-carousel-slide>
       <q-carousel-slide name="fourth">
         <q-video
           class="absolute-full"
-          src="https://www.youtube.com/embed/RVLNBVK8auM"
+          src="https://www.youtube.com/embed/ORMbqJH1fEg"
         />
       </q-carousel-slide>
     </q-carousel>
+
+    <div class="q-mt-sm">
+      <q-img :src="patternOneUrlStart" />
+    </div>
 
     <div class="container">
       <div class="container-item-1">
@@ -65,101 +84,94 @@
       </div>
     </div>
 
-    <!-- <q-carousel
-      v-model="secondSlide"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      arrows
-      animated
-      infinite
-      :autoplay="autoplaySlide"
-      @mouseover="autoplaySlide = false"
-      @mouseleave="autoplaySlide = true"
-      control-color="amber"
-      height="390px"
-      class="q-mt-lg"
-    >
-      <q-carousel-slide name="first" class="column no-wrap">
-        <div
-          class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-        >
-          <div
-            v-for="(image, index) in secondRowImageArrayOne"
-            class="image-container"
-            :style="{ backgroundImage: 'url(' + image.source + ')' }"
-            :key="index"
-          >
-            <p class="text-overlay">{{ image.name }}</p>
-          </div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" class="column no-wrap">
-        <div
-          class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-        >
-          <div
-            v-for="(image, index) in secondRowImageArrayTwo"
-            class="image-container"
-            :style="{ backgroundImage: 'url(' + image.source + ')' }"
-            :key="index"
-          >
-            <p class="text-overlay">{{ image.name }}</p>
-          </div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel> -->
-    <p class="q-mt-md text-center heading-text-class">Our Products</p>
-    <div class="q-mt-sm">
-      <q-carousel
-        v-model="thirdSlide"
-        transition-prev="slide-right"
-        transition-next="slide-left"
-        arrows
-        animated
-        infinite
-        :autoplay="autoplaySlide"
-        @mouseover="autoplaySlide = false"
-        @mouseleave="autoplaySlide = true"
-        control-color="amber"
-        height="390px"
-        class="q-mt-lg"
-      >
-        <q-carousel-slide name="first" class="column no-wrap">
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <div
-              v-for="(image, index) in secondRowImageArrayOne"
-              class="image-container"
-              :style="{ backgroundImage: 'url(' + image.source + ')' }"
-              :key="index"
-            >
-              <p class="text-overlay">{{ image.name }}</p>
-            </div>
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide name="second" class="column no-wrap">
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <div
-              v-for="(image, index) in secondRowImageArrayTwo"
-              class="image-container"
-              :style="{ backgroundImage: 'url(' + image.source + ')' }"
-              :key="index"
-            >
-              <p class="text-overlay">{{ image.name }}</p>
-            </div>
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
+    <div>
+      <q-img :src="patternOneUrlEnd" />
     </div>
+
+    <div class="our-range-class">
+      <p class="text-center heading-text-class">Our Products</p>
+      <div class="q-mt-sm" style="margin-left: 100px; margin-right: 100px">
+        <q-carousel
+          v-model="thirdSlide"
+          transition-prev="slide-right"
+          transition-next="slide-left"
+          arrows
+          animated
+          infinite
+          :autoplay="autoplaySlide"
+          @mouseover="autoplaySlide = false"
+          @mouseleave="autoplaySlide = true"
+          control-color="amber"
+          height="390px"
+          style="background-color: transparent"
+        >
+          <q-carousel-slide name="first" class="column no-wrap">
+            <div
+              class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
+            >
+              <div
+                v-for="(image, index) in secondRowImageArrayOne"
+                class="image-container"
+                :style="{ backgroundImage: 'url(' + image.source + ')' }"
+                :key="index"
+              >
+                <p class="text-overlay">{{ image.name }}</p>
+              </div>
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="second" class="column no-wrap">
+            <div
+              class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
+            >
+              <div
+                v-for="(image, index) in secondRowImageArrayTwo"
+                class="image-container"
+                :style="{ backgroundImage: 'url(' + image.source + ')' }"
+                :key="index"
+              >
+                <p class="text-overlay">{{ image.name }}</p>
+              </div>
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+    </div>
+
+    <div class="bottom-pattern-one-url-end-class"></div>
+
+    <div class="corporate-orders-class">
+      <div class="corporate-orders-container">
+        <div class="container-item-1">
+          <p class="corporate-orders-heading-text">Industries Orders</p>
+          <h2 class="corporate-orders-content-text-one">
+            Industries Bulk Orders for all festivals
+          </h2>
+          <p class="corporate-orders-content-text-two">
+            We are supplying for many industries around Housr and Bangalore. We
+            also maintain a good relationship with our clients
+          </p>
+          <q-btn
+            label="Contact"
+            style="margin-top: 10px; background-color: #0073bb; color: white"
+            @click="redirectToContactPage()"
+          />
+        </div>
+        <div class="corporate-offer-logo-class" style="margin-left: 50px">
+          <p>Sweets</p>
+          <span>Order</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom-pattern-one-url-end-class"></div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const topSlide = ref("first");
 const secondSlide = ref("first");
 const thirdSlide = ref("first");
@@ -167,6 +179,8 @@ const autoplaySlide = ref(true);
 const founderUrl = ref(
   "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQAJJE7aVa1WRAcBLjq8SB3Semenl3TsliJlMTTOu8ZzZAN84f9zN62WHEuP59cPTyPHWax-QX6day8vfg"
 );
+const patternOneUrlStart = ref("src/assets/images/pattern-start-deco1.png");
+const patternOneUrlEnd = ref("src/assets/images/pattern-end-deco1.png");
 
 const secondRowImageArrayOne = ref([
   {
@@ -189,16 +203,16 @@ const secondRowImageArrayOne = ref([
       "https://thumbs.dreamstime.com/z/pile-indian-sweets-motichoor-laddu-laddoo-71498478.jpg?w=992",
     name: "Laddu",
   },
-  {
-    source:
-      "https://www.shutterstock.com/image-photo/gulab-jamun-indian-pakistani-sweet-600w-2130427166.jpg",
-    name: "Gulab Jamun",
-  },
-  {
-    source:
-      "https://www.shutterstock.com/image-photo/besan-burfi-mohanthal-roasted-barfi-600w-2192226217.jpg",
-    name: "Mysore Paak",
-  },
+  // {
+  //   source:
+  //     "https://www.shutterstock.com/image-photo/gulab-jamun-indian-pakistani-sweet-600w-2130427166.jpg",
+  //   name: "Gulab Jamun",
+  // },
+  // {
+  //   source:
+  //     "https://www.shutterstock.com/image-photo/besan-burfi-mohanthal-roasted-barfi-600w-2192226217.jpg",
+  //   name: "Mysore Paak",
+  // },
 ]);
 
 const secondRowImageArrayTwo = ref([
@@ -222,17 +236,24 @@ const secondRowImageArrayTwo = ref([
       "https://www.yummytummyaarthi.com/wp-content/uploads/2011/10/IMG_7292.jpg",
     name: "Kaara Chevu",
   },
-  {
-    source:
-      "https://thumbs.dreamstime.com/z/spicy-indian-snack-kara-boondi-delicious-very-easy-to-make-60858041.jpg?w=992",
-    name: "Kaara Boondhi",
-  },
-  {
-    source:
-      "https://3.bp.blogspot.com/-i3oynvQnoL0/TqamZwyOdPI/AAAAAAAABqc/bb3tsdVgt8I/s1600/vada.jpg",
-    name: "Ribbon Pakkavada",
-  },
+  // {
+  //   source:
+  //     "https://thumbs.dreamstime.com/z/spicy-indian-snack-kara-boondi-delicious-very-easy-to-make-60858041.jpg?w=992",
+  //   name: "Kaara Boondhi",
+  // },
+  // {
+  //   source:
+  //     "https://3.bp.blogspot.com/-i3oynvQnoL0/TqamZwyOdPI/AAAAAAAABqc/bb3tsdVgt8I/s1600/vada.jpg",
+  //   name: "Ribbon Pakkavada",
+  // },
 ]);
+
+// methods
+const redirectToContactPage = () => {
+  router.push({
+    name: "contact-us",
+  });
+};
 </script>
 
 <style lang="sass" scoped>
@@ -276,10 +297,10 @@ const secondRowImageArrayTwo = ref([
   transform: scale(1.1)
 
 .heading-text-class
-  font-size: 35px
+  font-size: 45px
   color: #363636
-  font-family: 'Arial', sans-serif
-  font-weight: 450
+  font-family: 'Merriweather', serif
+  font-weight: 700
 
 .about-heading-class
   font-size: 45px
@@ -297,15 +318,102 @@ const secondRowImageArrayTwo = ref([
 .container
   display: flex
   justify-content: center
-  align-items: stretch
+  align-content: center
   flex-wrap: wrap
 
-.border-container
-  padding: 20px
-  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"><circle cx="10" cy="10" r="5"/></svg>')
-  background-repeat: repeat-x
-  background-size: 20px 20px
-  margin-top: 20px
+.our-range-class
+  background-image: url("src/assets/images/bg-image-yellow.jpg")
+  height: 100%
+  position: relative
+  padding-top: 100px
+  padding-bottom: 100px
+
+.bottom-pattern-one-url-end-class
+  position: relative
+  width: 100%
+  background-image: url('src/assets/images/pattern-end-deco1.png')
+  transform: translateX(-50%)
+  height: 50px
+  bottom: 0px
+  left:50%
+  top:-24px
+
+.corporate-orders-class
+  background-image: url("src/assets/images/background-offer-img.jpg")
+  background-attachment: fixed
+  background-position: center
+  background-repeat: no-repeat
+  background-size: cover
+
+.corporate-orders-container
+  height: 500px
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-wrap: wrap
+
+.corporate-orders-heading-text
+  font-size: 15px
+  font-family: 'Open Sans', sans-serif
+  font-weight: 600
+  text-align: left
+  color: #ffffff
+  margin-top: -9px
+  display: inline-block
+
+.corporate-orders-content-text-one
+  font-size: 45px
+  color: #ffffff
+  margin-top: 10px
+  margin-bottom: 30px
+  font-family: 'Merriweather', serif
+  font-weight: 700
+
+.corporate-orders-content-text-two
+  font-size: 15px
+  color: #ffffff
+  margin-bottom: 15px
+  font-family: 'Open Sans', sans-serif
+  font-weight: 550
+
+.corporate-offer-logo-class
+  color: #ffffff
+  border-radius: 50%
+  width: 220px
+  height: 220px
+  display: inline-block
+  text-align: center
+  line-height: 175px
+  background: linear-gradient(to right, #dac079 0%, #f1d486 100%)
+  z-index: 1
+  font-size: 35px
+  position: relative
+
+.corporate-offer-logo-class p
+  font-size: 57px
+  font-weight: 700
+
+.corporate-offer-logo-class span
+  font-size: 40px
+  position: absolute
+  top: 57px
+  left: 0
+  right: 0
+  font-weight: 600
+
+.corporate-offer-logo-class::after
+  position: absolute
+  content: ""
+  top: 0
+  left: 0
+  right: 0
+  bottom: 0
+  border: 2px dashed #ffc000
+  border-radius: 50%
+  z-index: 1
+  margin: -10px
+  transition: 0.5s
+  animation: rotation 50s infinite linear
 
 @media screen and (max-width: 600px)
   .text-overlay
@@ -314,4 +422,8 @@ const secondRowImageArrayTwo = ref([
 @media screen and (max-width: 1200px)
   .text-overlay
       font-size: 3vw
+
+@media screen and (max-width: 400px)
+  .text-overlay
+      font-size: 1vw
 </style>
